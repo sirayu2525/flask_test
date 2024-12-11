4a556b1d0a0034e093d504815991d3d989a72c01
sirayu2525 <taiyou1685@yahoo.ne.jp>
2024-12-11 14:14:12 +0900

    change

--- app/app.py
+++ app/app.py
@@ -1,22 +1,38 @@
-#Flaskとrender_template（HTMLを表示させるための関数）をインポート
-from flask import Flask,render_template
+# #Flaskとrender_template（HTMLを表示させるための関数）をインポート
+# from flask import Flask,render_template
 
-#Flaskオブジェクトの生成
+# #Flaskオブジェクトの生成
+# app = Flask(__name__)
+
+
+# #「/」へアクセスがあった場合に、"Hello World"の文字列を返す
+# @app.route("/")
+# def hello():
+#     return "Hello World"
+
+
+# #「/index」へアクセスがあった場合に、「index.html」を返す
+# @app.route("/index")
+# def index():
+#     return render_template("index.html")
+
+
+# #おまじない
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+from flask import Flask,render_template,request
+
 app = Flask(__name__)
 
 
-#「/」へアクセスがあった場合に、"Hello World"の文字列を返す
 @app.route("/")
-def hello():
-    return "Hello World"
-
-
-#「/index」へアクセスがあった場合に、「index.html」を返す
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    name = request.args.get("name")
+    okyo = ["色不異空","空不異色","色即是空","空即是色"]
+    return render_template("index.html",name=name,okyo=okyo)
 
 
-#おまじない
 if __name__ == "__main__":
     app.run(debug=True)
