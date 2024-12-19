3636fc05cdfc383c3a3f352a33bfa5520960bece
sirayu2525 <taiyou1685@yahoo.ne.jp>
2024-12-19 11:28:06 +0900

    Complete Day6

--- add_data.py
+++ add_data.py
@@ -1,17 +1,22 @@
 from models.database import db_session
-from models.models import OnegaiContent
+from models.models import OnegaiContent, User
+from sqlalchemy.exc import IntegrityError
 
 # データを作成
 c1 = OnegaiContent("お願いします", "5000兆円ください")
 c2 = OnegaiContent("助けてください", "ぽんぽんぺいん")
-c3 = OnegaiContent("許してください", "なんでもしますから")
+
+d1 = User("user1", "password1")
 
 # データベースに追加
-db_session.add(c1)
-db_session.add(c2)
-db_session.add(c3)
-
-# コミットして変更を保存
-db_session.commit()
-
-print("データを追加しました。")
+try:
+    db_session.add(c1)
+    db_session.add(c2)
+    db_session.add(d1)
+    # コミットして変更を保存
+    db_session.commit()
+    print("データを追加しました。")
+except IntegrityError as e:
+    db_session.rollback()
+    print("データの追加に失敗しました。重複するデータが存在します。")
+    print(e)
--- models/models.py
+++ models/models.py
@@ -17,3 +17,16 @@ class OnegaiContent(Base):
 
     def __repr__(self):
         return '<Title %r>' % (self.title)
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(128), unique=True)
+    hashed_password = Column(String(128))
+
+    def __init__(self, user_name=None, hashed_password=None):
+        self.user_name = user_name
+        self.hashed_password = hashed_password
+
+    def __repr__(self):
+        return '<Name %r>' % (self.user_name)
